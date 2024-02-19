21cec29cbfe3dc02a75b28898d023852308e9f97
vo73444 <vo73444@mail.harpercollege.edu>
2024-02-18 21:49:02 -0600

    add on

--- menu.cpp
+++ menu.cpp
@@ -2,23 +2,33 @@
 
 using namespace std;
 
-//function that takes in arguments which can be set by the caller
-int input_validation(int lower_bound, int upper_bound, string prompt, string error_message){
 
-    int input; // declares variable that user will eventually input
+int input_validation(int lower_bound, int upper_bound, string prompt, string error_message, string def){
+
+    string input;
 
     cout << prompt; // outputs prompt which caller has set in the parameters
 
-    cin >> input; //takes input from user and stores it in the input variable
+    getline(cin, input); //takes input from user and stores it in the input variable
 
+
+    if(input != "default"){
     //loop that error checks to see if the user entered a valid input
     //based on the bounds set by the caller
-    while (input > upper_bound || input < lower_bound){
-        cout << error_message << prompt;
-        cin >> input;
+        while (stoi(input) > upper_bound || stoi(input) < lower_bound){
+            cout << error_message << prompt;
+            getline(cin, input);
+            
+        }
+
+    } 
+
+    else{
+        
+        input = def;
     }
 
-    return input;
+    return stoi(input);
 }
 
 string input_validation(string prompt, string error_message){
@@ -39,11 +49,9 @@ string input_validation(string prompt, string error_message){
 }
 
 int main(){
-    int value = input_validation(0, 100, "Please enter a value: ", "Your value is invalid.\n\n\n");
+    int value = input_validation(0, 100, "Please enter a value. Enter 'default' to use the value 100: ", "Your value is invalid.\n\n\n", "100");
 
     cout << "The value chosen by the user is " << value << "\n\n";
-
-    cin.ignore();
 
     string input = input_validation("Please enter a string: ", "Your string is invalid");
 
